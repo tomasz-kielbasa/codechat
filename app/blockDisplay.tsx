27b96e083cf8fdfa23a1ac0cf8bd0948a580b1bc
Tomasz Kiełbasa <70tomek@gmail.com>
2024-01-09 22:35:26 +0100

    Rewrite interface to support chat

---
+++ app/blockDisplay.tsx
@@ -0,0 +1,49 @@
+import { CopyBlock, vs2015 } from "react-code-blocks";
+import Markdown from "react-markdown";
+
+type ReasoningBlocksProps = {
+  blocks: Array<{ [key: string]: string }>;
+}
+
+export const BlockDisplay = ({ blocks }: ReasoningBlocksProps) => {
+  return (
+    <div className="flex flex-col gap-2 my-2">
+      {blocks.map(({content, tag, id}) => {
+        if (tag === 'code') {
+          return <CopyBlock
+            text={content}
+            key={id}
+            theme={vs2015}
+            language="python"
+            codeBlock={true}
+          />
+        }
+        if (tag === 'output') {
+          // return <CopyBlock
+          //   text={content}
+          //   key={id}
+          //   theme={vs2015}
+          //   language="text"
+          //   showLineNumbers={false}
+          //   codeBlock={true}
+          // />
+          return (<pre key={id}>
+            {content}
+          </pre>)
+        }
+        if (tag === 'text') {
+          return <Markdown key={id}>{content}</Markdown>
+        }
+        if (tag === 'img') {
+          return <img
+            src={content}
+            className="w-full"
+          />
+        }
+        if (tag === 'instruction') {
+          return <p className="p-2 rounded bg-accent-foreground w-fit self-end">{content}</p>
+        }
+      })}
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,198 +1,262 @@
 'use client';
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
+import { TbCsv } from "react-icons/tb";
+import { IoSend } from "react-icons/io5";
+import { ImSpinner2 } from "react-icons/im";
+import { BlockDisplay } from "./blockDisplay";
+import { block, generateNewBlocks } from "./actions";
+import { useEffect, useRef, useState } from "react";
+import { PythonProvider, usePython } from "react-py";
 
-import React, {useState, useEffect, useRef} from "react";
-import { useCompletion } from "ai/react";
-import { ReasoningBlocks } from "./reasoningBlocks";
-import { PythonProvider, usePython, usePythonConsole } from "react-py";
-import { blockToString, extractBlocks, processCodeBlocks } from "@/utils";
+function processCodeBlocks(blocks: Array<{ [key: string]: string }>) : string {
+  function wrapCode (code: string) {
+    function wrapInTryCatch(pythonCode: string): string {
+      // Splitting the Python code into lines
+      const lines = pythonCode.split('\n');
+  
+      // Adding indentation to each line
+      const indentedPythonCode = lines.map(line => `    ${line}`).join('\n');
+  
+      const tryCatchTemplate = `
+import traceback
+import sys
 
+try:
+${indentedPythonCode}
+    print('', tag='end')
+except Exception as e:
+    print(f'Error: {e}')
+    print('', tag='error')
+`;
+    return tryCatchTemplate;
+  }
+
+  return `old_print = print
+def new_print(*args, tag='output', **kwargs):
+    old_print('{"tag": "' + tag +'", "content": "', end='')
+    old_print(*args, **kwargs)
+    old_print('"},', end=' ')
+print = new_print
+import os
+os.environ['MPLBACKEND'] = 'AGG'
+
+import base64
+from io import BytesIO
+
+import matplotlib.pyplot as plt
+
+# Patch
+def ensure_matplotlib_patch():
+  _old_show = plt.show
+
+  def show():
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    # Encode to a base64 str
+    img = 'data:image/png;base64,' + \
+    base64.b64encode(buf.read()).decode('utf-8')
+    # Write to stdout
+    print(img, tag='img')
+    plt.clf()
+
+  plt.show = show
+
+ensure_matplotlib_patch()
+# plt.figure(figsize=(7.68,5.76))
+
+import pandas as pd
+pd.set_option("display.max_columns", 40)
+pd.set_option("max_colwidth", 24)
+pd.set_option("display.width", 79)
+
+` + wrapInTryCatch(code);
+  }
+  // Filter out only code blocks and deep copy
+  const codeBlocks = JSON.parse(JSON.stringify(blocks.filter(block => block.tag === 'code' && !block.error))) as Array<block>;
+
+  // Process each block except the last one to remove print statements
+  for (let i = 0; i < codeBlocks.length - 1; i++) {
+    codeBlocks[i].content = codeBlocks[i].content.replace(/^\s*(sns\.|plt\.|print).*/gm, '')
+  }
+
+  // Concatenate all blocks into one string
+  console.log(codeBlocks.map(block => block.content).join('\n'))
+  return wrapCode(codeBlocks.map(block => block.content).join('\n'));
+}
 
 
 export default function Home() {
-  const {
-    completion,
-    input,
-    isLoading,
-    handleInputChange,
-    complete,
-  } = useCompletion({
-    api: '/api/completion',
-  });
 
-  const { runPython, stdout, stderr, isReady, writeFile } = usePython({
+  const [file, setFile] = useState<File | null>(null)
+  const [instruction, setInstruction] = useState('')
+  const [blocks, setBlocks] = useState<Array<block>>([])
+  const [isRunning, setIsRunning] = useState(false)
+  const blocksEndRef = useRef<null | HTMLDivElement>(null);
+  const { runPython, stdout, isReady, writeFile } = usePython({
     packages: {
-      official: ['pandas', 'matplotlib'],
+      official: ['pandas', 'matplotlib', 'scikit-learn'],
+      micropip: ['seaborn']
     }
   });
 
-  const [file, setFile] = useState<File | null>(null);
-  const [blocks, setBlocks] = useState<Array<{ [key: string]: string }>>([]);
-  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files ? e.target.files[0] : null
+    setFile(selectedFile)
+  };
 
-  const blocksEndRef = useRef<null | HTMLDivElement>(null);
-  
+  const handleInstructionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInstruction(e.target.value)
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    console.log(`Instruction: ${instruction}`)
+    console.log(`Filename: ${file?.name}`)
+    if (blocks.length > 0) {
+      setBlocks((prev) => [
+        ...prev,
+        {
+          content: instruction,
+          tag: 'instruction',
+          id: crypto.randomUUID()
+        }
+      ])
+    } else {
+      if (!file) {
+        alert('Upload a CSV')
+        return
+      }
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const content = e.target!.result as string;
+        
+        writeFile(file!.name, content);
+      };
+      reader.readAsText(file!);
+      setBlocks((prev) => [
+        ...prev,
+        {
+          content: file!.name,
+          tag: 'file',
+          id: crypto.randomUUID()
+        },
+        {
+          content: instruction,
+          tag: 'instruction',
+          id: crypto.randomUUID()
+        }
+      ])
+    }
+    setInstruction('')
+    setIsRunning(true)
+  }
+
   const scrollToBottom = () => {
     blocksEndRef.current?.scrollIntoView({ behavior: "smooth" })
   }
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = e.target.files ? e.target.files[0] : null;
-    setFile(selectedFile);
-  };
-  
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setIsRunning(true);
-    setBlocks([]);
-    const reader = new FileReader();
-    reader.onload = async (e) => {
-      const content = e.target!.result as string;
-      
-      writeFile(file!.name, content);
-    };
-    reader.readAsText(file!);
-    
-    setBlocks(blocks => [...blocks, {
-        tag: 'instruction',
-        content: input,
-        id: crypto.randomUUID(),
-      },
-      {
-        tag: 'file',
-        content: file!.name,
-        id: crypto.randomUUID(),
+  // Handles blocks changes, dispatches generation and execution
+  useEffect(() => {
+    scrollToBottom()
+    if (blocks.length > 0){
+      const lastTag = blocks[blocks.length - 1].tag
+      if (lastTag === 'instruction' || lastTag === 'output') {
+        const updateBlocks = async () => {
+          const newBlocks = await generateNewBlocks(blocks)
+          if (newBlocks.length > 0) {
+            setBlocks((prev) => [
+              ...prev,
+              ...newBlocks,
+            ])
+          } else {
+            setIsRunning(false)
+          }
+        }
+        updateBlocks()
+      } else if (lastTag === 'code') {
+        console.log('Running code...')
+        runPython(processCodeBlocks(blocks))
+      } else {
+        setIsRunning(false)
       }
-    ])
-  }
+    }
+  }, [blocks])
 
   // Handles stdout, when execution is finished, updates blocks
   useEffect(() => {
-    // console.log(stdout)
+    console.log(stdout)
     if (stdout.endsWith(', ')) {
-      const output: Array<{ [key: string]: string }> = JSON.parse('[' + stdout.replace(/\n/g, "\\n").slice(0, -2) + ']');
-      const last = output.pop()!;
-      if (last.tag == 'end') {
-        setBlocks((prev) => {return [
-          ...prev, ...output.map((block) => {
-            block.id = crypto.randomUUID();
-            block.content = block.content.replace(/\\n/g, "\n")
-            return block
-          })
-        ]})
+      try {
+        const output: Array<{ [key: string]: string }> = JSON.parse('[' + stdout.replace(/\n/g, "\\n").slice(0, -2) + ']');
+        const last = output.pop()!;
+        if (last.tag == 'end') {
+          setBlocks((prev) => {return [
+            ...prev, ...output.map((block) => {
+              block.id = crypto.randomUUID();
+              block.content = block.content.replace(/\\n/g, "\n")
+              return block as block
+            })
+          ]})
+        }
+        if (last.tag == 'error') {
+          setBlocks((prev) => {
+            const updatedBlocks = prev.map((block, index) => {
+              // If it's the last block, update its 'error' key
+              if (index === blocks.length - 1) {
+                return { ...block, error: '1' };
+              }
+              return block as block
+            });
+            return [
+            ...updatedBlocks, ...output.map((block) => {
+              block.id = crypto.randomUUID();
+              block.content = block.content.replace(/\\n/g, "\n")
+              return block as block
+            })
+          ]})
+        }
       }
-      if (last.tag == 'error') {
-        setBlocks((prev) => {
-          const updatedBlocks = prev.map((block, index) => {
-            // If it's the last block, update its 'error' key
-            if (index === blocks.length - 1) {
-              return { ...block, error: '1' };
-            }
-            return block;
-          });
-          return [
-          ...updatedBlocks, ...output.map((block) => {
-            block.id = crypto.randomUUID();
-            return block
-          })
-        ]})
+      catch {
+        0
       }
     }
-
-    
-  //   if (stdout.endsWith('ERROR\nEND OF EXECUTION\n')) {
-  //     setBlocks(blocks => {
-  //       const updatedBlocks = blocks.map((block, index) => {
-  //         // If it's the last block, update its 'error' key
-  //         if (index === blocks.length - 1) {
-  //           return { ...block, error: '1' };
-  //         }
-  //         return block;
-  //       });
-      
-  //       // Add the new block as before
-  //       return [...updatedBlocks, {
-  //         tag: 'output',
-  //         content: stdout.slice(0, -'ERROR\nEND OF EXECUTION\n'.length).trimEnd().replace(/^\n+/, ""),
-  //         id: crypto.randomUUID(),
-  //       }];
-  //     });
-  //   } else if (stdout.endsWith('END OF EXECUTION\n')) {
-  //     setBlocks(blocks => [...blocks, {
-  //       tag: 'output',
-  //       content: stdout.slice(0, -'END OF EXECUTION\n'.length).trimEnd().replace(/^\n+/, ""),
-  //       id: crypto.randomUUID(),
-  //     }])
-  //   }
   }, [stdout])
-
-
-  // When completion is finished updates blocks
-  useEffect(() => {
-    if (!isLoading && completion.trim().length > 0) {
-      // console.log(completion);
-      setBlocks(blocks => [...blocks, ...extractBlocks(completion)]);
-    }
-  }, [isLoading]);
   
-  // Handles blocks changes, dispatches code execution or completion
-  useEffect(() => {
-    console.log(blocks);
-    scrollToBottom();
-    if (blocks.length > 0 && blocks.length < 15) {
-      if (blocks[blocks.length - 1].tag === 'code') {
-        const codeToRun = processCodeBlocks(blocks);
-        console.log(codeToRun);
-        console.log('Running python...');
-        runPython(codeToRun);
-      } else if (blocks[blocks.length - 1].tag === 'file' || blocks[blocks.length - 1].tag === 'output'){
-        const prompt = blocks.map(block => blockToString(block)).join('\n');;
-        console.log('Running completion...');
-        // console.log(prompt);
-        complete(prompt);
-      } else {
-        setIsRunning(false);
-      }
-    }
-
-  }, [blocks]);
-
-  // console.log(stderr);
-  // console.log(stdout);
   return (
     <PythonProvider>
-      <div className="max-w-3xl mx-auto">
-        <form className="my-2" onSubmit={handleSubmit}>
-          <Label htmlFor="csv-file">Upload your CSV here</Label>
-          <Input
-            id="csv-file"
-            type="file"
-            accept=".csv"
-            required={true}
-            onChange={handleFileChange}
-          />
-          <Label htmlFor="instruction">Enter your instruction</Label>
-          <Textarea
-            id="instruction"
-            className="mt-2"
-            placeholder="What is the highest...?"
-            value={input}
-            onChange={handleInputChange}
-            required={true}
-          />
-          <Button className="w-full mt-2" type="submit" disabled={isRunning || !isReady}>
-            Go!
-          </Button>
-        </form>
-        <ReasoningBlocks blocks={blocks} />
-        <div ref={blocksEndRef} />
-        {isLoading && <ReasoningBlocks blocks={extractBlocks(completion)} />}
-      </div>
-      {/* <pre>{blocks.map(block => blockToString(block)).join('\n')}</pre> */}
+      <main className='flex flex-col h-screen max-w-3xl mx-auto'>
+        <div className="flex-1 overflow-y-auto">
+          <BlockDisplay blocks={blocks} />
+          <div ref={blocksEndRef} />
+        </div>
+        {blocks.length > 0 && <button className="border w-24 p-1 rounded-full self-end" onClick={() => setBlocks([])}>Clear chat</button>}
+          <form className="flex content-between justify-between my-2 px-2 gap-2 border rounded-full items-center" onSubmit={handleSubmit}>
+            {blocks.length === 0 && <>
+              <input
+                type="file"
+                name="file"
+                id="file"
+                accept=".csv"
+                onChange={handleFileChange}
+                hidden
+              />
+              <label htmlFor="file" className="flex items-center justify-center w-12 h-12 rounded-full border hover:cursor-pointer"><TbCsv size={25}/></label>
+            </>}
+
+            <textarea
+              name="instruction"
+              onChange={handleInstructionChange}
+              value={instruction}
+              className="p-2 h-16 flex-1 bg-inherit focus:border-none focus:outline-none focus:ring-0"
+              placeholder="Type your message..." />
+            {isRunning || !isReady ? (
+              <ImSpinner2 className='mr-1 animate-spin' size={25}/>
+            ) : (
+              <button type="submit" className="mr-1" disabled={isRunning || !isReady}><IoSend size={25}/></button>
+            )}
+          </form>
+      </main>
     </PythonProvider>
   )
 }
